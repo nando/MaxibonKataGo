32532ae115524e4f0472bcbee44f3d59b4c638ea
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-14 17:54:38 +0100

    developer always grab positive maxibons property (code)
    > go test ./...                                                        rapid [80f2f7d] (!)
    ok      _/home/nando/src/lab/MaxibonKataGo/src
    >

--- src/developer.go
+++ src/developer.go
@@ -1,10 +1,12 @@
 package developer
 
+import( "math" )
+
 type Developer struct {
     name string
     maxibons_to_grab int
 }
 
 func (dev Developer) maxibonsToGrab() int {
-    return dev.maxibons_to_grab
+    return int( math.Max( +0, float64(dev.maxibons_to_grab )))
 }
