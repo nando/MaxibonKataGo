d8d5ba7b51b21e3a3b2aea868b6cbaca493b8c77
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-14 17:41:31 +0100

    remove gopter implementation to do TDD with rapid

--- src/developer.go
+++ src/developer.go
@@ -1,12 +1,10 @@
 package developer
 
-import( "math" )
-
 type Developer struct {
     name string
     maxibons_to_grab int
 }
 
 func (dev Developer) maxibonsToGrab() int {
-    return int( math.Max( +0, float64(dev.maxibons_to_grab )))
+    return dev.maxibons_to_grab
 }
