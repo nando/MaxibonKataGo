1fc5d0f58c0db1a9f761b1102874b6bf59dde9c5
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-15 12:09:29 +0100

    assign developer's name in "construction" property...
    DISCLAIMER: I'm using this kata as my first contact with Go!... has any
    sense to test this property in Go!?? :thinking_face: Thanks in advance!!
    <3<3<3

--- src/developer_test.go
+++ src/developer_test.go
@@ -14,4 +14,13 @@ func TestDeveloperProperties(t *testing.T) {
       t.Fatalf("developer should always grab a positive number of maxibons.")
     }
   }, rapid.IntsRange(-100, 100))
+
+  // Property #2: developer should assign the name of the developer in "construction".
+  rapid.Check( t, func( t *rapid.T, name string ) {
+    developer := Developer{ name, 1 }
+
+    if developer.name != name {
+      t.Fatalf("developer should assign the name of the developer in 'construction'")
+    }
+  }, rapid.Strings())
 }
