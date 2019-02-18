0664b8b4dba5d14953dbe87284961eefc314e902
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-18 10:07:14 +0100

    assign # of maxibons to every developer (code)

--- src/developer.go
+++ src/developer.go
@@ -10,3 +10,13 @@ type Developer struct {
 func (dev Developer) maxibonsToGrab() int {
     return int( math.Max( +0, float64(dev.maxibons_to_grab )))
 }
+
+func Developers() (developers map[ string ]Developer) {
+  developers = make( map[string]Developer )
+  developers[ "pedro" ]  = Developer{ "Pedro", 3 }
+  developers[ "fran" ]   = Developer{ "Fran", 1 }
+  developers[ "davide" ] = Developer{ "Davide", 0 }
+  developers[ "sergio" ] = Developer{ "Sergio", 2 }
+  developers[ "jorge" ]  = Developer{ "Jorge", 1 }
+  return
+}
