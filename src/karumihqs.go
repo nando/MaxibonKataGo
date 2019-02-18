575a7ffbe5a3bb21608e83f41827b07b1c7bca97
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-18 18:28:47 +0100

    always more than 2 maxibons in the fridge property (test+code)

--- src/karumihqs.go
+++ src/karumihqs.go
@@ -1,5 +1,10 @@
 package maxibonkata
 
+import( "math" )
+
+func KarumiHQs_MIN_MAXIBONS() int { return 3 }
+func KarumiHQs_BUY_MAXIBONS() int { return 10 }
+
 type KarumiHQs struct {
   name string
   maxibons_left int
@@ -11,6 +16,29 @@ func NewKarumiHQs( name string ) (karumihqs KarumiHQs) {
   return
 }
 
-func ( hqs KarumiHQs ) maxibonsLeft() int {
+func ( hqs *KarumiHQs ) maxibonsLeft() int {
+  return hqs.maxibons_left
+}
+
+func ( hqs *KarumiHQs ) openFridge( dev Developer ) int {
+  hqs.grabMaxibons( dev )
+
+  if hqs.maxibons_left < KarumiHQs_MIN_MAXIBONS() {
+    hqs.buyMaxibons()
+  }
+
+  return hqs.maxibons_left
+}
+
+func ( hqs *KarumiHQs ) grabMaxibons( dev Developer ) int {
+  hqs.maxibons_left = int( math.Max( +0,
+                                     float64( hqs.maxibons_left - dev.maxibonsToGrab())))
+
+  return hqs.maxibons_left
+}
+
+func ( hqs *KarumiHQs ) buyMaxibons() int {
+  hqs.maxibons_left =+ KarumiHQs_BUY_MAXIBONS()
+
   return hqs.maxibons_left
 }
--- src/karumihqs_test.go
+++ src/karumihqs_test.go
@@ -14,4 +14,16 @@ func TestKarumiHQsProperties(t *testing.T) {
       t.Fatalf("new KarumiHQs instances should start the day with 10 maxibons.")
     }
   }, rapid.Strings())
+
+  // Property #2: should always has more than two maxibons in the fridge.
+  rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
+    hqs := NewKarumiHQs( "The Cocktail Oviedo" )
+    developer := Developer{ "Ada", maxibons_to_grab }
+
+    hqs.openFridge( developer )
+
+    if hqs.maxibonsLeft() < 3 {
+      t.Fatalf("should always has more than two maxibons in the fridge.")
+    }
+  }, rapid.IntsRange(0, 42))
 }
