eea76e40443fca1d5174be3d48389d44774e65c4
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-20 19:17:02 +0100

    indent w tabs instead of two spaces

--- src/karumihqs.go
+++ src/karumihqs.go
@@ -6,44 +6,44 @@ func KarumiHQs_MIN_MAXIBONS() int { return 3 }
 func KarumiHQs_BUY_MAXIBONS() int { return 10 }
 
 type KarumiHQs struct {
-  name string
-  maxibons_left int
-  melting_maxibons int
+	name string
+	maxibons_left int
+	melting_maxibons int
 }
 
 func NewKarumiHQs( name string ) (karumihqs KarumiHQs) {
-  karumihqs.name = name
-  karumihqs.maxibons_left = 10
-  return
+	karumihqs.name = name
+	karumihqs.maxibons_left = 10
+	return
 }
 
 func ( hqs *KarumiHQs ) maxibonsLeft() int {
-  return hqs.maxibons_left
+	return hqs.maxibons_left
 }
 
 func ( hqs *KarumiHQs ) openFridge( dev Developer ) int {
-  hqs.grabMaxibons( dev )
+	hqs.grabMaxibons( dev )
 
-  if hqs.maxibons_left < KarumiHQs_MIN_MAXIBONS() {
-    hqs.buyMaxibons()
-  }
+	if hqs.maxibons_left < KarumiHQs_MIN_MAXIBONS() {
+		hqs.buyMaxibons()
+	}
 
-  return hqs.maxibons_left
+	return hqs.maxibons_left
 }
 
 func ( hqs *KarumiHQs ) meltingMaxibons() int {
-  return hqs.melting_maxibons
+	return hqs.melting_maxibons
 }
 
 func ( hqs *KarumiHQs ) grabMaxibons( dev Developer ) int {
-  hqs.maxibons_left = int( math.Max( +0,
-                                     float64( hqs.maxibons_left - dev.maxibonsToGrab())))
-  return hqs.maxibons_left
+	hqs.maxibons_left = int( math.Max(+0,
+																		float64( hqs.maxibons_left - dev.maxibonsToGrab())))
+	return hqs.maxibons_left
 }
 
 func ( hqs *KarumiHQs ) buyMaxibons() int {
-  hqs.melting_maxibons = hqs.maxibons_left
-  hqs.maxibons_left =+ KarumiHQs_BUY_MAXIBONS()
+	hqs.melting_maxibons = hqs.maxibons_left
+	hqs.maxibons_left =+ KarumiHQs_BUY_MAXIBONS()
 
-  return hqs.maxibons_left
+	return hqs.maxibons_left
 }
--- src/karumihqs_test.go
+++ src/karumihqs_test.go
@@ -1,54 +1,53 @@
 package maxibonkata
 
 import (
-  "testing"
-  "github.com/flyingmutant/rapid"
+	"testing"
+	"github.com/flyingmutant/rapid"
 )
 
 func TestKarumiHQsProperties(t *testing.T) {
-  // Property #1: office should start the day with 10 maxibons.
-  rapid.Check( t, func( t *rapid.T, name string ) {
-    hqs := NewKarumiHQs( name )
+	// Property #1: office should start the day with 10 maxibons.
+	rapid.Check( t, func( t *rapid.T, name string ) {
+		hqs := NewKarumiHQs( name )
 
-    if hqs.maxibonsLeft() != 10 {
-      t.Fatalf("new KarumiHQs instances should start the day with 10 maxibons.")
-    }
-  }, rapid.Strings())
+		if hqs.maxibonsLeft() != 10 {
+			t.Fatalf("new KarumiHQs instances should start the day with 10 maxibons.")
+		}
+	}, rapid.Strings())
 
-  // Property #2: should always has more than two maxibons in the fridge.
-  rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
-    hqs := NewKarumiHQs( "The Cocktail Oviedo" )
-    developer := Developer{ "Ada", maxibons_to_grab }
+	// Property #2: should always has more than two maxibons in the fridge.
+	rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
+		hqs := NewKarumiHQs( "The Cocktail Oviedo" )
+		developer := Developer{ "Ada", maxibons_to_grab }
 
-    hqs.openFridge( developer )
+		hqs.openFridge( developer )
 
-    if hqs.maxibonsLeft() < 3 {
-      t.Fatalf("should always has more than two maxibons in the fridge.")
-    }
-  }, rapid.IntsRange(0, 42))
+		if hqs.maxibonsLeft() < 3 {
+			t.Fatalf("should always has more than two maxibons in the fridge.")
+		}
+	}, rapid.IntsRange(0, 42))
 
-  // Property #3: should buy 10 more maxibons if there are less than 3 in the fridge
-  rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
-    hqs := NewKarumiHQs( "The Cocktail DF" )
-    developer := Developer{ "Frida", maxibons_to_grab }
+	// Property #3: should buy 10 more maxibons if there are less than 3 in the fridge
+	rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
+		hqs := NewKarumiHQs( "The Cocktail DF" )
+		developer := Developer{ "Frida", maxibons_to_grab }
 
-    hqs.openFridge( developer )
+		hqs.openFridge( developer )
 
-    if hqs.maxibonsLeft() < 10 {
-      t.Fatalf("should buy 10 more maxibons if there are less than 3 in the fridge.")
-    }
-  }, rapid.IntsRange(8, 42))
+		if hqs.maxibonsLeft() < 10 {
+			t.Fatalf("should buy 10 more maxibons if there are less than 3 in the fridge.")
+		}
+	}, rapid.IntsRange(8, 42))
 
-  // Property #4: maxibons above BUY_MAXIBONS are melting maxibons
-  rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
-    hqs := NewKarumiHQs( "The Cocktail Santiago" )
-    developer := Developer{ "Violeta", maxibons_to_grab }
+	// Property #4: maxibons above BUY_MAXIBONS are melting maxibons
+	rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
+		hqs := NewKarumiHQs( "The Cocktail Santiago" )
+		developer := Developer{ "Violeta", maxibons_to_grab }
 
-    hqs.openFridge( developer )
+		hqs.openFridge( developer )
 
-    if hqs.meltingMaxibons() == 0 {
-      t.Fatalf("maxibons above BUY_MAXIBONS should be melting maxibons.")
-    }
-  }, rapid.IntsRange(8, 9)) // => buy_maxibons() ==> 1 or 2 melting...
-
+		if hqs.meltingMaxibons() == 0 {
+			t.Fatalf("maxibons above BUY_MAXIBONS should be melting maxibons.")
+		}
+	}, rapid.IntsRange(8, 9)) // => buy_maxibons() ==> 1 or 2 melting...
 }
