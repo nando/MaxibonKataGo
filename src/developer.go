cf7f19c6a338ea34dd22195f30aad96503d16e21
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-20 09:32:50 +0100

    add Tck's developers
    + indent w tabs instead of two spaces

--- src/developer.go
+++ src/developer.go
@@ -3,20 +3,30 @@ package maxibonkata
 import( "math" )
 
 type Developer struct {
-    name string
-    maxibons_to_grab int
+	name string
+	maxibons_to_grab int
 }
 
 func (dev Developer) maxibonsToGrab() int {
-    return int( math.Max( +0, float64(dev.maxibons_to_grab )))
+	return int( math.Max( +0, float64(dev.maxibons_to_grab )))
 }
 
 func Developers() (developers map[ string ]Developer) {
-  developers = make( map[string]Developer )
-  developers[ "pedro" ]  = Developer{ "Pedro", 3 }
-  developers[ "fran" ]   = Developer{ "Fran", 1 }
-  developers[ "davide" ] = Developer{ "Davide", 0 }
-  developers[ "sergio" ] = Developer{ "Sergio", 2 }
-  developers[ "jorge" ]  = Developer{ "Jorge", 1 }
-  return
+	developers = make( map[string]Developer )
+	// Karumi's
+	developers[ "pedro" ]  = Developer{ "Pedro", 3 }
+	developers[ "fran" ]   = Developer{ "Fran", 1 }
+	developers[ "davide" ] = Developer{ "Davide", 0 }
+	developers[ "sergio" ] = Developer{ "Sergio", 2 }
+	developers[ "jorge" ]  = Developer{ "Jorge", 1 }
+	// Tck's
+	developers[ "nuria" ]  = Developer{ "Nuria", 3 }
+	developers[ "fausto" ] = Developer{ "Fausto", 2 }
+	developers[ "julia" ]  = Developer{ "Julia",	0 }
+	developers[ "luismi" ] = Developer{ "Luismi", 3 }
+	developers[ "susana" ] = Developer{ "Susana", 2 }
+	developers[ "sahu" ]   = Developer{ "Sahu", 1 }
+	developers[ "vero" ]   = Developer{ "Vero", 3 }
+	developers[ "vito" ]   = Developer{ "Vito", 3 }
+	return
 }
--- src/developer_test.go
+++ src/developer_test.go
@@ -1,44 +1,54 @@
 package maxibonkata
 
 import (
-  "testing"
-  "github.com/flyingmutant/rapid"
+	"testing"
+	"github.com/flyingmutant/rapid"
 )
 
 func TestDeveloperProperties(t *testing.T) {
-  // Property #1: developer should always grab a positive number of maxibons.
-  rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
-    developer := Developer{ "Ada", maxibons_to_grab }
+	// Property #1: developer should always grab a positive number of maxibons.
+	rapid.Check( t, func( t *rapid.T, maxibons_to_grab int ) {
+		developer := Developer{ "Ada", maxibons_to_grab }
 
-    if developer.maxibonsToGrab() < 0 {
-      t.Fatalf("developer should always grab a positive number of maxibons.")
-    }
-  }, rapid.IntsRange(-100, 100))
+		if developer.maxibonsToGrab() < 0 {
+			t.Fatalf("developer should always grab a positive number of maxibons.")
+		}
+	}, rapid.IntsRange(-100, 100))
 
-  // Property #2: developer should assign the name of the developer in "construction".
-  rapid.Check( t, func( t *rapid.T, name string ) {
-    developer := Developer{ name, 1 }
+	// Property #2: developer should assign the name of the developer in "construction".
+	rapid.Check( t, func( t *rapid.T, name string ) {
+		developer := Developer{ name, 1 }
 
-    if developer.name != name {
-      t.Fatalf("developer should assign the name of the developer in 'construction'")
-    }
-  }, rapid.Strings())
+		if developer.name != name {
+			t.Fatalf("developer should assign the name of the developer in 'construction'")
+		}
+	}, rapid.Strings())
 }
 
 // Test #3: should assign the number of maxibons specified to every developer.
 func Test_the_number_of_maxibons_specified_to_every_developer(t *testing.T) {
-  developers := Developers()
-  maxibons := make(map[string]int)
-  maxibons[ "pedro" ]  = 3
-  maxibons[ "fran" ]   = 1
-  maxibons[ "davide" ] = 0
-  maxibons[ "sergio" ] = 2
-  maxibons[ "jorge" ]  = 1
-  for key, dev := range developers {
-    if dev.maxibonsToGrab() != maxibons[ key ] {
-      t.Fatalf("developer %s should grab %d maxibons.\n",
-               developers[ key ].name,
-               maxibons[ key ])
-    }
-  }
+	developers := Developers()
+	maxibons := make(map[string]int)
+
+	maxibons[ "pedro" ]  = 3
+	maxibons[ "fran" ]	 = 1
+	maxibons[ "davide" ] = 0
+	maxibons[ "sergio" ] = 2
+	maxibons[ "jorge" ]  = 1
+	maxibons[ "nuria" ]  = 3
+	maxibons[ "fausto" ] = 2
+	maxibons[ "julia" ]  = 0
+	maxibons[ "luismi" ] = 3
+	maxibons[ "susana" ] = 2
+	maxibons[ "sahu" ]   = 1
+	maxibons[ "vero" ]   = 3
+	maxibons[ "vito" ]   = 3
+
+	for key, dev := range developers {
+		if dev.maxibonsToGrab() != maxibons[ key ] {
+			t.Fatalf("developer %s should grab %d maxibons.\n",
+							 developers[ key ].name,
+							 maxibons[ key ])
+		}
+	}
 }
